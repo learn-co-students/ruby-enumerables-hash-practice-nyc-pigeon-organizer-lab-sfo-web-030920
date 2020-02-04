568670d3029c1657acd452e1bc0c4566d6426d87
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-02-04 22:20:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+
+
+# pigeon_data.each do |key, value|
+#   puts "#{key} ----> #{value}"
+#     value.each do |new_val, names|
+#     puts "#{new_val} ---- #{names}"
+#       names.each do |name|
+#         puts name
+#       end
+#     end
+#   end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new_hash = {}
+  data.each do |key, value|
+    value.each do |new_val, names|
+      names.each do |name|
+      new_hash[name] = {} unless new_hash[name]
+      new_hash[name][key] = [] unless new_hash[name][key]
+        new_hash[name][key] << new_val.to_s
+      end
+    end
+  end
+  pp new_hash
 end
