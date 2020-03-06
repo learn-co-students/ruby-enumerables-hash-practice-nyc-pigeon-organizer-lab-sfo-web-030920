11e8774435af41db1a20042250bb372018b66ab5
Cindy Cai <caicindy87@gmail.com>
2020-03-06 17:41:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attributes, info|
+   info.each do |color_gender_locations, name|
+  
+     name.each do |pigeon_name|
+      if !pigeon_list[pigeon_name]
+        pigeon_list[pigeon_name] = {}
+      end
+      if !pigeon_list[pigeon_name][attributes]
+        pigeon_list[pigeon_name][attributes] = []
+      end
+      pigeon_list[pigeon_name][attributes].push(color_gender_locations.to_s)
+      
+    end
+   end
+ end
+ pigeon_list
 end
+
+
+
+
+# data.each do |attribute, info|
+#     info.each do |color, name|
+#       pigeon_list[color[name]] = {}
+#       binding.pry
+#     end
+#   end
